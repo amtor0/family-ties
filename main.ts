cd9670ff9cd57f9561e2c77707bce9e05f2dabbb
amtor0 <112511616+amtor0@users.noreply.github.com>
2022-11-08 11:59:22 -0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     Direction = "UP"
-    erygeu.setImage(img`
+    animation.runImageAnimation(
+    erygeu,
+    [img`
         . . . . . . f f f f . . . . . . 
         . . . . f f e e e e f f . . . . 
         . . . f e e e f f e e e f . . . 
@@ -17,91 +19,165 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
         . . . . . f f f f f f . . . . . 
         . . . . . f f . . f f . . . . . 
-        `)
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f f 2 f e f . . 
+        . . f f f 2 f e e 2 2 f f f . . 
+        . . f e 2 f f e e 2 f e e f . . 
+        . f f e f f e e e f e e e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . . e f f f f f f f f 4 e . . 
+        . . . 4 f 2 2 2 2 2 e d d 4 . . 
+        . . . e f f f f f f e e 4 . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . . f f f f 2 2 f f f f . . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e f 2 f f f 2 f 2 e f . . 
+        . . f f f 2 2 e e f 2 f f f . . 
+        . . f e e f 2 e e f f 2 e f . . 
+        . f f e e e f e e e f f e f f . 
+        . f f e e e e e e e e e e f f . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f e . . . 
+        . . 4 d d e 2 2 2 2 2 f 4 . . . 
+        . . . 4 e e f f f f f f e . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    100,
+    true
+    )
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (Direction == "DOWN") {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 2 1 2 . . . . . . 
-            . . . . . . . 2 1 2 . . . . . . 
-            . . . . . . . 2 1 2 . . . . . . 
-            . . . . . . . 3 1 3 . . . . . . 
-            . . . . . . 2 3 1 3 2 . . . . . 
-            . . . . . . 2 1 1 1 2 . . . . . 
-            . . . . . . 2 1 1 1 3 . . . . . 
-            . . . . . . 3 1 1 1 3 . . . . . 
-            . . . . . . 3 1 1 1 3 . . . . . 
-            . . . . . . 3 1 1 1 3 . . . . . 
-            . . . . . . 2 3 1 3 2 . . . . . 
-            . . . . . . . 2 2 2 . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, erygeu, 0, 500000)
-    } else if (Direction == "UP") {
-        projectile2 = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 2 2 2 . . . . . . 
-            . . . . . . 2 3 1 3 2 . . . . . 
-            . . . . . . 3 1 1 1 3 . . . . . 
-            . . . . . . 3 1 1 1 3 . . . . . 
-            . . . . . . 3 1 1 1 3 . . . . . 
-            . . . . . . 2 1 1 1 3 . . . . . 
-            . . . . . . 2 1 1 1 2 . . . . . 
-            . . . . . . 2 3 1 3 2 . . . . . 
-            . . . . . . . 3 1 3 . . . . . . 
-            . . . . . . . 2 1 2 . . . . . . 
-            . . . . . . . 2 1 2 . . . . . . 
-            . . . . . . . 2 1 2 . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, erygeu, 0, -500000)
-    } else if (Direction == "LEFT") {
-        projectile2 = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . 2 2 2 2 . . . . . . . . . 
-            . . 2 1 1 1 1 2 2 . . . . . . . 
-            . . 1 1 1 1 1 1 3 3 2 2 . . . . 
-            . . 1 1 1 1 1 1 1 1 3 3 3 3 . . 
-            . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
-            . . 1 1 1 1 1 1 1 3 2 2 3 3 . . 
-            . . 2 1 1 1 1 3 2 2 . . . . . . 
-            . . . 2 2 2 2 . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, erygeu, -500000, 0)
-    } else if (Direction == "RIGHT") {
-        projectile2 = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . 2 2 2 2 . . . 
-            . . . . . . . 2 2 1 1 1 1 2 . . 
-            . . . . 2 2 3 3 1 1 1 1 1 1 . . 
-            . . 3 3 3 3 1 1 1 1 1 1 1 1 . . 
-            . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
-            . . 3 3 2 2 3 1 1 1 1 1 1 1 . . 
-            . . . . . . 2 2 3 1 1 1 1 2 . . 
-            . . . . . . . . . 2 2 2 2 . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, erygeu, 5000000, 0)
-    }
-    bad_boy.setFlag(SpriteFlag.AutoDestroy, true)
+    timer.debounce("action", 150, function () {
+        if (Direction == "DOWN") {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . 2 1 2 . . . . . . 
+                . . . . . . . 2 1 2 . . . . . . 
+                . . . . . . . 2 1 2 . . . . . . 
+                . . . . . . . 3 1 3 . . . . . . 
+                . . . . . . 2 3 1 3 2 . . . . . 
+                . . . . . . 2 1 1 1 2 . . . . . 
+                . . . . . . 2 1 1 1 3 . . . . . 
+                . . . . . . 3 1 1 1 3 . . . . . 
+                . . . . . . 3 1 1 1 3 . . . . . 
+                . . . . . . 3 1 1 1 3 . . . . . 
+                . . . . . . 2 3 1 3 2 . . . . . 
+                . . . . . . . 2 2 2 . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, erygeu, 0, 500000)
+        } else if (Direction == "UP") {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . 2 2 2 . . . . . . 
+                . . . . . . 2 3 1 3 2 . . . . . 
+                . . . . . . 3 1 1 1 3 . . . . . 
+                . . . . . . 3 1 1 1 3 . . . . . 
+                . . . . . . 3 1 1 1 3 . . . . . 
+                . . . . . . 2 1 1 1 3 . . . . . 
+                . . . . . . 2 1 1 1 2 . . . . . 
+                . . . . . . 2 3 1 3 2 . . . . . 
+                . . . . . . . 3 1 3 . . . . . . 
+                . . . . . . . 2 1 2 . . . . . . 
+                . . . . . . . 2 1 2 . . . . . . 
+                . . . . . . . 2 1 2 . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, erygeu, 0, -500000)
+        } else if (Direction == "LEFT") {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . 2 2 2 2 . . . . . . . . . 
+                . . 2 1 1 1 1 2 2 . . . . . . . 
+                . . 1 1 1 1 1 1 3 3 2 2 . . . . 
+                . . 1 1 1 1 1 1 1 1 3 3 3 3 . . 
+                . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+                . . 1 1 1 1 1 1 1 3 2 2 3 3 . . 
+                . . 2 1 1 1 1 3 2 2 . . . . . . 
+                . . . 2 2 2 2 . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, erygeu, -500000, 0)
+        } else if (Direction == "RIGHT") {
+            projectile2 = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . 2 2 2 2 . . . 
+                . . . . . . . 2 2 1 1 1 1 2 . . 
+                . . . . 2 2 3 3 1 1 1 1 1 1 . . 
+                . . 3 3 3 3 1 1 1 1 1 1 1 1 . . 
+                . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+                . . 3 3 2 2 3 1 1 1 1 1 1 1 . . 
+                . . . . . . 2 2 3 1 1 1 1 2 . . 
+                . . . . . . . . . 2 2 2 2 . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, erygeu, 5000000, 0)
+        }
+    })
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     Direction = "LEFT"
-    erygeu.setImage(img`
+    animation.runImageAnimation(
+    erygeu,
+    [img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 2 2 e d d 4 . . . . . 
+        . . . f 2 2 2 e d d e . . . . . 
+        . . . f 5 5 4 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
         . . . . . . . . . . . . . . . . 
         . . . . f f f f f f . . . . . . 
         . . . f 2 f e e e e f f . . . . 
@@ -118,7 +194,44 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . f f 5 5 f e e f f f . . . . 
         . . f f f f f f f f f f . . . . 
         . . . f f f . . . f f . . . . . 
-        `)
+        `,img`
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e e f f . . . . 
+        . . . f 2 2 2 e d d 4 . . . . . 
+        . . . f 2 2 2 e d d e . . . . . 
+        . . . f 5 5 4 f e e f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d d 4 e e e f . . . 
+        . . . f e 4 4 4 e d d 4 . . . . 
+        . . . f 2 2 2 2 e d d e . . . . 
+        . . f f 5 5 4 4 f e e f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `],
+    100,
+    true
+    )
 })
 function createlevel () {
     if (level == 1) {
@@ -127,6 +240,8 @@ function createlevel () {
     } else if (level == 2) {
         tiles.setCurrentTilemap(tilemap`level6`)
         tiles.placeOnRandomTile(erygeu, sprites.dungeon.floorMixed)
+    } else if (level == 3) {
+        tiles.setCurrentTilemap(tilemap`level7`)
     }
     for (let index = 0; index < 4; index++) {
         bad_boy = sprites.create(img`
@@ -159,9 +274,65 @@ function createlevel () {
         tiles.placeOnRandomTile(bad_boy, sprites.dungeon.floorLight0)
     }
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileDarkGrass3, function (sprite, location) {
+    clearlevel()
+})
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     Direction = "RIGHT"
-    erygeu.setImage(img`
+    animation.runImageAnimation(
+    erygeu,
+    [img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e e e d d d f . . . 
+        . . . . . f 4 d d e 4 e f . . . 
+        . . . . . f e d d e 2 2 f . . . 
+        . . . . f f f e e f 5 5 f f . . 
+        . . . . f f f f f f f f f f . . 
+        . . . . . f f . . . f f f . . . 
+        `,img`
+        . . . . . . f f f f f f . . . . 
+        . . . . f f e e e e f 2 f . . . 
+        . . . f f e e e e f 2 2 2 f . . 
+        . . . f e e e f f e e e e f . . 
+        . . . f f f f e e 2 2 2 2 e f . 
+        . . . f e 2 2 2 f f f f e 2 f . 
+        . . f f f f f f f e e e f f f . 
+        . . f f e 4 4 e b f 4 4 e e f . 
+        . . f e e 4 d 4 1 f d d e f . . 
+        . . . f e e e 4 d d d d f . . . 
+        . . . . f f e e 4 4 4 e f . . . 
+        . . . . . 4 d d e 2 2 2 f . . . 
+        . . . . . e d d e 2 2 2 f . . . 
+        . . . . . f e e f 4 5 5 f . . . 
+        . . . . . . f f f f f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
         . . . . . . . . . . . . . . . . 
         . . . . . . f f f f f f . . . . 
         . . . . f f e e e e f 2 f . . . 
@@ -178,24 +349,40 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . f e e f 4 4 5 5 f f . . 
         . . . . f f f f f f f f f f . . 
         . . . . . f f . . . f f f . . . 
-        `)
+        `],
+    100,
+    true
+    )
 })
-function CreateLevel1 () {
-    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
-        erygeu.destroy()
-    }
-    for (let value of sprites.allOfKind(SpriteKind.Player)) {
-        bad_boy.destroy()
-    }
-}
 function clearlevel () {
     for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
         value.destroy()
     }
+    level += 1
+    createlevel()
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     Direction = "DOWN"
-    erygeu.setImage(img`
+    animation.runImageAnimation(
+    erygeu,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
         . . . . . . . . . . . . . . . . 
         . . . . . . f f f f . . . . . . 
         . . . . f f f 2 2 f f f . . . . 
@@ -212,18 +399,56 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . f 4 4 5 5 f e e . . . . 
         . . . . f f f f f f f . . . . . 
         . . . . f f f . . . . . . . . . 
-        `)
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . e f e 4 d d d d 4 e f . . . 
+        . . e 4 d d e 2 2 2 2 f e f . . 
+        . . . e d d e 2 2 2 2 f 4 e . . 
+        . . . . e e f 5 5 4 4 f . . . . 
+        . . . . . f f f f f f f . . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    100,
+    true
+    )
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tileGrass2, function (sprite, location) {
     clearlevel()
-    createlevel()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 100)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     game.over(false)
 })
 let bad_boy: Sprite = null
 let projectile2: Sprite = null
-let projectile: Sprite = null
 let Direction = ""
 let erygeu: Sprite = null
 let level = 0
